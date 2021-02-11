568b6fb2ed659aaea999307d39df5e63627b122d
duarte_ <gabrielleduartee3@gmail.com>
2021-02-11 16:11:35 -0300

    First commit

---
+++ src/test/java/triangle/test/TriangleTest.java
@@ -0,0 +1,15 @@
+package triangle.test;
+
+import org.junit.Test;
+
+import br.ufrn.imd.triangle.Triangle;
+
+public class TriangleTest {
+
+	Triangle triangle = new Triangle();//Integer s1, Integer s2, Integer s3
+	
+	@Test
+	public void equalSidesMethodTrue() {
+		
+	}
+}
